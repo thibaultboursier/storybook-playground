e5ca9ce2ac1df3088d0d4ed67f6b86cd81e88e37
thibaultboursier <boursier.thibault@gmail.com>
2018-08-15 22:59:46 +0200

    chore: require all stories inside src/components folder

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
 import { configure } from '@storybook/react';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const req = require.context('../src/components/', true, /stories\.jsx$/)
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(req);
 }
 
 configure(loadStories, module);
