1d703b0954a550991e330d8f225668613c1e8ea7
thibaultboursier <boursier.thibault@gmail.com>
2018-08-16 21:48:48 +0200

    feat(messages-area): use Knobs addon

--- src/components/messages-area/MessagesArea.stories.jsx
+++ src/components/messages-area/MessagesArea.stories.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import uuidv4 from 'uuid/v4';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import { MessagesArea } from './MessagesArea';
 
 const messages = [{ id: uuidv4(), content: 'Hello' }];
 
 storiesOf('Chat', module)
+    .addDecorator(withKnobs)
     .add('MessagesArea', () => (
-        <MessagesArea messages={messages} />)
+        <MessagesArea messages={object('messages', messages)} />)
     );
